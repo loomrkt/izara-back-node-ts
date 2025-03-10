59c2e2403ac868051c5c39ed103417db10af758b
RAKOTONDRAZAKA Nameno Fanantenana <mrnrakotondrazaka@gmail.com>
2025-03-10 14:07:48 +0300

    user controller supabase

--- api/controllers/user.controller.ts
+++ api/controllers/user.controller.ts
@@ -1,29 +1,31 @@
 import { Request, Response } from 'express';
-import client from '../../utils/database';
+import { supabase } from "../../utils/database";
 
 interface User {
   id: string;
 }
 
-export const getUserProfile =  async (req: Request, res: Response) => {
-    try {
-      
-      if (!req.user) {
-         res.status(401).json({ message: 'Unauthorized' });
-         return;
-      }
-      const userId = (req.user as User).id;
-
-      const { rows } = await client.query('SELECT * FROM users WHERE id = $1', [userId]);
-
-      if (rows.length === 0) {
-         res.status(404).json({ message: 'User not found' });
-         return;
-      }
-
-      res.status(200).json(rows[0]); // Retourner le profil de l'utilisateur
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Internal server error' });
+export const getUserProfile = async (req: Request, res: Response) => {
+  try {
+    if (!req.user) {
+      res.status(401).json({ message: "Unauthorized" });
+      return;
     }
-  };
+    const userId = (req.user as User).id;
+
+    const { data: user, error } = await supabase
+      .from("users")
+      .select("*")
+      .eq("id", userId)
+      .single(); // Récupère un seul enregistrement
+
+    if (error || !user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user); // Retourner le profil de l'utilisateur
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
